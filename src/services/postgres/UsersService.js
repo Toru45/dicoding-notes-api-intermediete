03a19b5a5956f65c672bc57b87c79d1efaf47a52
Toru45 <faiqradiansyah@gmail.com>
2025-02-19 23:26:10 +0700

    fix: :bug: Wrong response in users api when users adding existed username

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-underscore-dangle */
-const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
@@ -12,13 +11,13 @@ class UsersService {
 
   async addUser({ username, password, fullname }) {
     await this.verifyNewUsername(username);
-
     const id = `user-${nanoid(16)}`;
     const hashedPassword = await bcrypt.hash(password, 10);
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
       values: [id, username, hashedPassword, fullname],
     };
+
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
@@ -36,7 +35,7 @@ class UsersService {
     const result = await this._pool.query(query);
 
     if (result.rows.length > 0) {
-      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
 
@@ -47,9 +46,11 @@ class UsersService {
     };
 
     const result = await this._pool.query(query);
+
     if (!result.rows.length) {
       throw new NotFoundError('User tidak ditemukan');
     }
+
     return result.rows[0];
   }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable camelcase */
 const mapDBToModel = ({
   id,
   title,
