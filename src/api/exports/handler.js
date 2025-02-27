d282ee7f0e4e2535bc686f9e535857c306906316
Toru45 <faiqradiansyah@gmail.com>
2025-02-27 21:32:14 +0700

    feat: exports notes implemented using RabbitMQ

---
+++ src/api/exports/handler.js
@@ -0,0 +1,30 @@
+class ExportsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postExportNotesHandler = this.postExportNotesHandler.bind(this);
+  }
+
+  async postExportNotesHandler(request, h) {
+    // validasi request.payload
+    this._validator.validateExportNotesPayload(request.payload);
+
+    const message = {
+      userId: request.auth.credentials.id,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    // ubah objek message dalam bentuk string
+    await this._service.sendMessage('export:notes', JSON.stringify(message));
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda dalam antrean',
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
---
+++ src/services/rabbitmq/ProducerService.js
@@ -0,0 +1,20 @@
+const amqp = require('amqplib');
+
+const ProducerService = {
+  sendMessage: async (queue, message) => {
+    const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+    const channel = await connection.createChannel();
+    // membuat queue
+    await channel.assertQueue(queue, {
+      durable: true,
+    });
+
+    // kirim pesan ke dalam bentuk buffer
+    await channel.sendToQueue(queue, Buffer.from(message));
+    setTimeout(() => {
+      connection.close();
+    }, 1000);
+  },
+};
+
+module.exports = ProducerService;
